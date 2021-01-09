a96aee0eee8e01e76f9c74688fab1dbe0a235a71
Bruno Bega Harnik <brunoharnik@gmail.com>
2021-01-09 19:21:27 -0300

    Add footer

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,21 +1,24 @@
 import './styles.css';
 import { ReactComponent as MainImage } from './main.svg';
+import Footer from '../Footer';
 
 function Home() {
     return (
-        <section className="home-container">
-            <div className="home-content">
-                <div className="home-actions">
-                    <h2 className="home-title">Faça seu pedido que entregamos para você!</h2>
-                    <h3 className="home-subtitle">Escolha o seu pedido e, em minutos, levaremos até sua casa.</h3>
-                    <a className="home-btn-order" href="orders">Fazer pedido</a>
+        <>
+            <section className="home-container">
+                <div className="home-content">
+                    <div className="home-actions">
+                        <h2 className="home-title">Faça seu pedido que entregamos para você!</h2>
+                        <h3 className="home-subtitle">Escolha o seu pedido e, em minutos, levaremos até sua casa.</h3>
+                        <a className="home-btn-order" href="orders">Fazer pedido</a>
+                    </div>
+                    <div className="home-image">
+                        <MainImage />
+                    </div>
                 </div>
-                <div className="home-image">
-                    <MainImage />
-                </div>
-            </div>
-
-        </section>
+            </section>
+            <Footer />
+        </>
     )
 }
 
