a66371573400bbca8daa6f3b6211e108e03a0a73
Bruno Bega Harnik <brunoharnik@gmail.com>
2021-01-09 19:00:32 -0300

    Add Home page

---
+++ front-web/src/Home/index.tsx
@@ -0,0 +1,22 @@
+import './styles.css';
+import { ReactComponent as MainImage } from './main.svg';
+
+function Home() {
+    return (
+        <section className="home-container">
+            <div className="home-content">
+                <div className="home-actions">
+                    <h2 className="home-title">Faça seu pedido que entregamos para você!</h2>
+                    <h3 className="home-subtitle">Escolha o seu pedido e, em minutos, levaremos até sua casa.</h3>
+                    <a className="home-btn-order" href="orders">Fazer pedido</a>
+                </div>
+                <div className="home-image">
+                    <MainImage />
+                </div>
+            </div>
+
+        </section>
+    )
+}
+
+export default Home;
