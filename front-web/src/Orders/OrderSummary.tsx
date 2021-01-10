e989a3ab70b7c6c73f38a3b380a774ff6a56b717
Bruno Bega Harnik <brunoharnik@gmail.com>
2021-01-10 16:52:06 -0300

    Finish front-web

---
+++ front-web/src/Orders/OrderSummary.tsx
@@ -0,0 +1,29 @@
+type Props = {
+    amount: number;
+    totalPrice: number;
+    onSubmit: () => void;
+}
+
+function OrderSummary( { amount, totalPrice, onSubmit} : Props) {
+    return (
+        <div className="order-summary-container">
+            <div className="order-summary-content">
+                <div>
+                    <span className="amount-selected-container">
+                        <strong className="amount-selected">{amount}</strong>
+                    Pedidos selecionados
+                    </span>
+                    <span className="order-summary-total">
+                        <strong className="amount-selected">R$ {totalPrice}</strong>
+                    valor total
+                    </span>
+                </div>
+                <button onClick={onSubmit} className="order-summary-make-order">
+                    Finalizar pedido
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default OrderSummary;
